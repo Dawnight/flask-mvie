3cf6670f93f63d3cf1217c6e5913601958f9dacd
Dawnight <weizhiqimail@gmail.com>
2019-01-05 20:21:15 +0800

    评论的查看和删除

--- app/admin/views.py
+++ app/admin/views.py
@@ -2,7 +2,7 @@
 from flask import render_template, redirect, url_for, flash, session, request
 from . import admin
 from app.admin.forms import LoginForm, TagForm, MovieForm, PreviewForm
-from app.models import Admin, Tag, Movie, Preview, User
+from app.models import Admin, Tag, Movie, Preview, User, Comment
 from app import db, app
 from functools import wraps
 from werkzeug.utils import secure_filename
@@ -308,10 +308,28 @@ def user_del(id=None):
     flash('删除用户成功', 'ok')
     return redirect(url_for('admin.user_list', page=1))
 
-@admin.route('/comment/list')
+
+@admin.route('/comment/list/<int:page>/', methods=['GET'])
 @admin_login_req
-def comment_list():
-    return render_template('admin/comment_list.html')
+def comment_list(page):
+    if page is None:
+        page = 1
+    page_data = Comment.query.join(Movie)\
+        .join(User)\
+        .filter(Movie.id == Comment.movie_id, User.id == Comment.user_id)\
+        .order_by(Comment.id)\
+        .paginate(page=page, per_page=10)
+    return render_template('admin/comment_list.html', page_data=page_data)
+
+
+@admin.route('/comment/del/<int:id>', methods=['GET'])
+@admin_login_req
+def comment_del(id=None):
+    comment = Comment.query.filter_by(id=id).first_or_404()
+    db.session.delete(comment)
+    db.session.commit()
+    flash('删除评论成功', 'ok')
+    return redirect(url_for('admin.comment_list', page=1))
 
 
 @admin.route('/moviecol/list')
