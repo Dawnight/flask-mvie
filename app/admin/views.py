32ee9625224f56e00cbecd19d614aca4ef8c48af
Dawnight <weizhiqimail@gmail.com>
2019-01-05 17:34:34 +0800

    预告片的查看，修改和删除

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from flask import render_template, redirect, url_for, flash, session, request
 from . import admin
-from app.admin.forms import LoginForm, TagForm, MovieForm
-from app.models import Admin, Tag, Movie
+from app.admin.forms import LoginForm, TagForm, MovieForm, PreviewForm
+from app.models import Admin, Tag, Movie, Preview
 from app import db, app
 from functools import wraps
 from werkzeug.utils import secure_filename
@@ -215,16 +215,72 @@ def movie_edit(id=None):
     return render_template('admin/movie_edit.html', form=form, movie=movie)
 
 
-@admin.route('/preview/add')
+@admin.route('/preview/add', methods=['GET', 'POST'])
 @admin_login_req
 def preview_add():
-    return render_template('admin/preview_add.html')
+    form = PreviewForm()
+    if form.validate_on_submit():
+        data = form.data
+        if not os.path.exists(app.config['UP_DIR']):
+            os.makedirs(app.config['UP_DIR'])
+            os.chmod(app.config['UP_DIR'], 0o770)
+        logo = change_filename(form.logo.data.filename)
+        form.logo.data.save(app.config['UP_DIR'] + logo)
+        preview = Preview(
+            title=data['title'],
+            logo=logo,
+        )
+        db.session.add(preview)
+        db.session.commit()
+        flash('添加预告片成功', 'ok')
+        return redirect(url_for('admin.preview_add'))
+    return render_template('admin/preview_add.html', form=form)
 
 
-@admin.route('/preview/list')
+@admin.route('/preview/list/<int:page>')
 @admin_login_req
-def preview_list():
-    return render_template('admin/preview_list.html')
+def preview_list(page=None):
+    if page is None:
+        page = 1
+    page_data = Preview.query.order_by(Preview.id).paginate(page=page, per_page=10)
+    return render_template('admin/preview_list.html', page_data=page_data)
+
+
+@admin.route('/preview/del/<int:id>', methods=['GET'])
+@admin_login_req
+def preview_del(id=None):
+    preview = Preview.query.filter_by(id=id).first_or_404()
+    db.session.delete(preview)
+    db.session.commit()
+    flash('删除预告片成功', 'ok')
+    return redirect(url_for('admin.preview_list', page=1))
+
+
+@admin.route('/preview/edit/<int:id>', methods=['GET', 'POST'])
+@admin_login_req
+def preview_edit(id=None):
+    form = PreviewForm()
+    form.submit.label.text = "修改"
+    form.logo.validators = []
+    preview = Preview.query.get_or_404(id)
+    if request.method == 'GET':
+        form.title.data = preview.title
+    if form.validate_on_submit():
+        data = form.data
+        if not os.path.exists(app.config['UP_DIR']):
+            os.makedirs(app.config['UP_DIR'])
+            os.chmod(app.config['UP_DIR'], 0o770)
+        if form.logo.data != "":
+            preview.logo = change_filename(form.logo.data.filename)
+            form.logo.data.save(app.config["UP_DIR"] + preview.logo)
+        preview.title = data['title']
+        db.session.add(preview)
+        db.session.commit()
+        flash('修改预告片成功', 'ok')
+        redirect(url_for('admin.preview_edit', id=id))
+    print(form)
+    print(preview)
+    return render_template('admin/preview_edit.html', form=form, preview=preview)
 
 
 @admin.route('/user/view')
